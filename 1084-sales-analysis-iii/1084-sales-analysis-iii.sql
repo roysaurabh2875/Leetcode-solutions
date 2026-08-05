d4b1bb3a1fb9844db46d93fc71f8f2a50ef78509
roysaurabh2875 <roysaurabh2875@gmail.com>
2026-08-05 15:42:02 +0530

    Time: 1219 ms (54.25%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT p.product_id, p.product_name 
+FROM Product p
+JOIN Sales s ON s.product_id = p.product_id
+WHERE s.sale_date BETWEEN '2019-01-01' AND '2019-03-31' AND 
+    p.product_id NOT IN 
+    (SELECT product_id 
+    FROM Sales 
+    WHERE sale_date < '2019-01-01' OR sale_date > '2019-03-31'
+    )
+GROUP BY p.product_id , p.product_name ;
