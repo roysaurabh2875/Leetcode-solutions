34ba3bbb9851750b47b3df4efd1757335e08962e
roysaurabh2875 <roysaurabh2875@gmail.com>
2026-08-17 11:49:44 +0530

    Time: 1 ms (61.09%), Space: 43.3 MB (32.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,26 +3,25 @@ class Solution {
         if (strs == null || strs.length == 0)
             return "";
 
-        // StringBuilder prefix = new StringBuilder();
-
-        // for (int i = 0; i < strs[0].length(); i++) {
-        //     char ch = strs[0].charAt(i);
-
-        //     for (int j = 0; j < strs.length; j++) {
-        //         if (i >= strs[j].length() || strs[j].charAt(i) != ch)
-        //             return prefix.toString();
-        //     }
-        //     prefix.append(ch);
+        // Arrays.sort(strs);
+        // String s1 = strs[0];
+        // String s2 = strs[strs.length -1];
+        // int i = 0;
+        // while(i < s1.length() && i < s2.length() && s1.charAt(i) == s2.charAt(i)){
+        //     i++;
         // }
-        // return prefix.toString();
-        Arrays.sort(strs);
-        String s1 = strs[0];
-        String s2 = strs[strs.length -1];
-        int i = 0;
-        while(i < s1.length() && i < s2.length() && s1.charAt(i) == s2.charAt(i)){
-            i++ ;
+        // return s1.substring(0,i);
+
+        //vertical scanning appproach 
+        for(int i = 0;i < strs[0].length();i++){
+            char ch = strs[0].charAt(i);
+
+            for(int j = 1;j < strs.length ;j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != ch){
+                    return strs[0].substring(0,i);
+                }
+            }
         }
-        if(i == 0) return "";
-        return s1.substring(0,i);
+        return strs[0];
     }
 }
