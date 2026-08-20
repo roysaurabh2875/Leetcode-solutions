42ef77d4428165845bc4571a157938bab8df9572
roysaurabh2875 <roysaurabh2875@gmail.com>
2026-08-20 11:57:34 +0530

    Time: 2 ms (57.45%), Space: 46.7 MB (66.35%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -3,13 +3,8 @@ class Solution {
         if(nums.length <= 2) return nums ;
         List<Integer> l1 = new ArrayList<>() ;
         List<Integer> l2 = new ArrayList<>() ;
-        for(int i = 0; i < 2 ;i++){
-            if(i % 2 == 0){
-                l1.add(nums[i]);
-            }else {
-                l2.add(nums[i]);
-            }
-        }
+        l1.add(nums[0]);
+        l2.add(nums[1]);
         for(int i = 2;i < nums.length ;i++){
             if(l1.get(l1.size()-1)>l2.get(l2.size()-1)){
                 l1.add(nums[i]);
