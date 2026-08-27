32f7f7de46ee07afe8ddde830621e196c3a1bf7c
roysaurabh2875 <roysaurabh2875@gmail.com>
2026-08-27 11:55:56 +0530

    Time: 3145 ms (5.03%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT t.id ,CASE 
+                WHEN t.p_id IS NULL THEN 'Root' 
+                WHEN EXISTS(SELECT 1 FROM Tree child WHERE child.p_id = t.id)THEN 'Inner'
+                ELSE 'Leaf'
+            END AS type
+FROM Tree t
+ORDER BY t.id ;
