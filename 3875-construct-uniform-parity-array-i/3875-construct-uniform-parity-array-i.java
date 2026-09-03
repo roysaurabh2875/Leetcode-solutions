f3a2a433ababb116e6740881f4d527f9629c6e5b
roysaurabh2875 <roysaurabh2875@gmail.com>
2026-09-03 19:24:48 +0530

    Time: 0 ms (100%), Space: 44.9 MB (87.77%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -3,3 +3,21 @@ class Solution {
         return true ;
     }
 }
+// int odd = 0;
+// boolean hasOdd = false ;
+// for(int i = nums1){
+//     if(i % 2 != 0){
+//         hasOdd = true ;
+//         odd = i ;
+//         break ;
+//     }
+// }
+// if(!hasOdd){
+//     return true ;
+// }
+// for(int i = 0;i < nums1.length ;i++){
+//     if(nums1[i] %2 == 0){
+//         nums1[i] = nums1[i] - odd ;
+//     }
+// }
+// return true ;
